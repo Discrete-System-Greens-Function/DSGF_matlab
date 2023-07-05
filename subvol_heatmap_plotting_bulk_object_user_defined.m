4ea2587f411508d8bc13a701b69f21bf35725377
Livia Correa <44705597+liviacorrea@users.noreply.github.com>
2023-07-05 09:36:54 -0700

    Add files via upload
    power density with user defined discretization

--- subvol_heatmap_plotting_bulk_object_user_defined.m
+++ subvol_heatmap_plotting_bulk_object_user_defined.m
@@ -1,4 +1,6 @@
-function subvol_heatmap_user_defined(r, L_sub, Q_total_subvol, c_limits, show_axes, output, saveDir, N, N1)
+function subvol_heatmap_plotting_bulk_object_user_defined(r, L_sub, Q_total_subvol,  Q_density_subvol, c_limits, c_limits_density, abs_limit_density, show_axes, output, saveDir, N, N1)
+%subvol_heatmap_user_defined
+
 % plots the subvolume heatmap in 2 different views
 %	
 %	Inputs:
@@ -18,16 +20,17 @@ function subvol_heatmap_user_defined(r, L_sub, Q_total_subvol, c_limits, show_ax
 
 	% Subvolume heat map for full particles (VIEW 1)
 	heatmap_view_1 = figure(4);
+    axis_y = 'Heat dissipated in a subvolume, Q [W]';
 	%subplot(1,2,1)
 	%[vert, fac] = voxel_image( r(1:N1,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(1:N1).' ); % Absorber (T = 0 K)
 	%[vert, fac] = voxel_image( r(N1+1:end,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(N1+1:end).' ); % Emitter (T = 300 K)
 	
     %[vert, fac] = voxel_image( r, L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol.', c_limits );
         
-	[vert, fac] = voxel_image( r(1:N_ref,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(:,1:N_ref), c_limits );
-    [vert, fac] = voxel_image( r(N_ref+1:N1,:), L_sub(N1), [], [], [], [], 'heatmap', Q_total_subvol(:,N_ref+1:N1), c_limits );
-    [vert, fac] = voxel_image( r(N1+1:N-N_ref,:), L_sub(N1+1), [], [], [], [], 'heatmap', Q_total_subvol(:,N1+1:N-N_ref), c_limits );
-    [vert, fac] = voxel_image( r(N-N_ref+1:N,:), L_sub(N), [], [], [], [], 'heatmap', Q_total_subvol(:,N-N_ref+1:N), c_limits );
+	[vert, fac] = voxel_image( r(1:N_ref,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(:,1:N_ref), c_limits, axis_y );
+    [vert, fac] = voxel_image( r(N_ref+1:N1,:), L_sub(N1), [], [], [], [], 'heatmap', Q_total_subvol(:,N_ref+1:N1), c_limits, axis_y );
+    [vert, fac] = voxel_image( r(N1+1:N-N_ref,:), L_sub(N1+1), [], [], [], [], 'heatmap', Q_total_subvol(:,N1+1:N-N_ref), c_limits, axis_y );
+    [vert, fac] = voxel_image( r(N-N_ref+1:N,:), L_sub(N), [], [], [], [], 'heatmap', Q_total_subvol(:,N-N_ref+1:N), c_limits, axis_y );
     
 	xlabel('x-axis (m)');
 	ylabel('y-axis (m)');
@@ -39,19 +42,23 @@ function subvol_heatmap_user_defined(r, L_sub, Q_total_subvol, c_limits, show_ax
 	end
 	%view(2)
 	view(-30,35)
+    
+    
+    norm_Q_density_subvol = Q_density_subvol'./abs_limit_density;
 
 	% Subvolume heat map for full particles (VIEW 2)
 	heatmap_view_2 = figure(5);
+    axis_y = 'Normalized heat density in a subvolume';
 	%subplot(1,2,2)
 	%[vert, fac] = voxel_image( r(1:N1,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(1:N1).' ); % Absorber (T = 0 K)
 	%[vert, fac] = voxel_image( r(N1+1:end,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(N1+1:end).' ); % Emitter (T = 300 K)
     
     %[vert, fac] = voxel_image( r, L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol.', c_limits );
     
-    [vert, fac] = voxel_image( r(1:N_ref,:), L_sub(1), [], [], [], [], 'heatmap', Q_total_subvol(:,1:N_ref), c_limits );
-    [vert, fac] = voxel_image( r(N_ref+1:N1,:), L_sub(N1), [], [], [], [], 'heatmap', Q_total_subvol(:,N_ref+1:N1), c_limits );
-    [vert, fac] = voxel_image( r(N1+1:N-N_ref,:), L_sub(N1+1), [], [], [], [], 'heatmap', Q_total_subvol(:,N1+1:N-N_ref), c_limits );
-    [vert, fac] = voxel_image( r(N-N_ref+1:N,:), L_sub(N), [], [], [], [], 'heatmap', Q_total_subvol(:,N-N_ref+1:N), c_limits );
+    [vert, fac] = voxel_image( r(1:N_ref,:), L_sub(1), [], [], [], [], 'heatmap', norm_Q_density_subvol(:,1:N_ref), c_limits_density, axis_y );
+    [vert, fac] = voxel_image( r(N_ref+1:N1,:), L_sub(N1), [], [], [], [], 'heatmap', norm_Q_density_subvol(:,N_ref+1:N1), c_limits_density, axis_y );
+    [vert, fac] = voxel_image( r(N1+1:N-N_ref,:), L_sub(N1+1), [], [], [], [], 'heatmap', norm_Q_density_subvol(:,N1+1:N-N_ref), c_limits_density, axis_y );
+    [vert, fac] = voxel_image( r(N-N_ref+1:N,:), L_sub(N), [], [], [], [], 'heatmap', norm_Q_density_subvol(:,N-N_ref+1:N), c_limits_density, axis_y );
         
 	xlabel('x-axis (m)');
 	ylabel('y-axis (m)');
@@ -68,8 +75,8 @@ function subvol_heatmap_user_defined(r, L_sub, Q_total_subvol, c_limits, show_ax
 	
 	if output.save_fig
 		
-	    heatmap_view_1_path = [saveDir '/heatmap_full_view1'];
-	    heatmap_view_2_path = [saveDir '/heatmap_full_view2'];
+	    heatmap_view_1_path = [saveDir '/heatmap_full_power'];
+	    heatmap_view_2_path = [saveDir '/heatmap_full_density'];
 
 	    saveas(heatmap_view_1, heatmap_view_1_path, string(output.figure_format))
 	    saveas(heatmap_view_2, heatmap_view_2_path, string(output.figure_format))
--- subvol_heatmap_plotting_user_defined.m
+++ subvol_heatmap_plotting_user_defined.m
@@ -1,4 +1,4 @@
-function subvol_heatmap_plotting_user_defined(r, L_sub, Q_total_subvol, show_axes, output, saveDir,  N, N1)
+function subvol_heatmap_plotting_user_defined(r, L_sub, Q_total_subvol, Q_density_subvol, show_axes, output, saveDir,  N, N1)
 % plots the heatmaps for the total heat dissipation per subvolume
 % plots generated:
 % 	heatmap for bulk object (2 different views)
@@ -13,10 +13,16 @@ function subvol_heatmap_plotting_user_defined(r, L_sub, Q_total_subvol, show_axe
 	abs_limit = max(abs(Q_total_subvol));
 	c_limits = [-abs_limit, abs_limit];
 	%c_limits = [min(Q_total_subvol), max(Q_total_subvol)];
+    
+    
+    abs_limit_density = max(abs(Q_density_subvol));
+    c_limits_density = [-abs_limit_density/abs_limit_density, abs_limit_density/abs_limit_density];
 
 
 	% plotting the subvolume heatmap in 2 seperate views
-	subvol_heatmap_plotting_bulk_object_user_defined(r, L_sub, Q_total_subvol, c_limits, show_axes, output, saveDir, N, N1);
+	subvol_heatmap_plotting_bulk_object_user_defined(r, L_sub, Q_total_subvol, Q_density_subvol, c_limits, c_limits_density, abs_limit_density, show_axes, output, saveDir, N, N1);
+    %subvol_heatmap_plotting_bulk_object(r, L_sub, Q_total_subvol, Q_density_subvol, c_limits, c_limits_density, abs_limit_density, show_axes, output, saveDir,ind_bulk,r_1,r_2,N);
+
 
     
     if output.heatmap_sliced
