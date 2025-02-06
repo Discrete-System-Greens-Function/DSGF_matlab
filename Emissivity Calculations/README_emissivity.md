4faa8f4ccf00ca9998d0ec3afdeefded787ee48d
Joseph McKay <80215756+jmckay1511@users.noreply.github.com>
2025-02-06 11:44:34 -0700

    Add files via upload

---
+++ Emissivity Calculations/README_emissivity.md
@@ -0,0 +1,43 @@
+MATLAB DSGF code
+
+Last Update: Feb 6, 2025
+
+The DSGF (discrete system Green's function) is a numerical framework to simulate radiative heat transfer between micro/nanostructured objects. The DSGF method is based on fluctuational electrodynamics and is applicable to near-field and far-field problems. The DSGF method and solver were developed at the University of Utah. 
+
+
+1. SYSTEM REQUIREMENT
+
+   A MATLAB license from MathWorks is required. The DSGF code has been tested on MATLAB version R2021a. 
+
+2. INSTALLATION
+
+   No installation is required. Download the DSGF_matlab source code.
+
+3. DEMO AND INSTRUCTIONS FOR USE
+   
+   The only file to be modified is: DSGF_user_inputs.
+
+   Step 1: Write a description of your simulation. The description will be available in the results table.
+
+   Step 2: Select the discretization_type between a ‘sample’ or a ‘user_defined’ simulation. ‘Sample’ is used for simulations with spheres, dipoles, and cubes. ‘User_defined’ is used for simulations with membranes.
+
+   Step 3: Define discretization for your simulation. Different parameters need to be modified depending on your selection in Step 2.
+        For the ‘sample’ provided, modify: discretization, L_char, and d. 
+    
+   Step 4: Select material. 
+
+   Step 5: Define dielectric function of the background reference medium.
+
+   Step 6: Define the frequency discretization. The option uniform_lambda is defined in terms of wavelength [m] while the options uniform_omega and non_uniform_omega are defined in angular frequency [rad/s]. 
+
+   Step 7: Define the only one temperature required for the power emitted calculation.
+
+   Step 8: Select outputs of the simulation.
+
+   Step 9: In the MATLAB editor, press the Run button.
+   
+   Key Assumptions: This code solves for the spectral, hemispherical emissivity for objects uniformly discretized, having the same size, shape, equal edge-to-edge spacing, and normalized to the same cross-sectional area. 
+		    If the user wants to run emissivity calculations on objects of different sizes, shapes, and cross-sectional areas, the user must modify the DSGF_Thermal_Emission code to account for the differences. 
+
+
+   The results are stored in a folder named with the time the simulation was launched.
---
+++ Emissivity Calculations/direct_function.m
@@ -0,0 +1,71 @@
+function [ G_sys_2D, Trans, Q_omega_bulk, Q_omega_subvol ] = direct_function( omega, r, epsilon, epsilon_ref, delta_V, T_vector, ind_bulk, wave_type )
+
+% This function calculates the system Green's function, the monochromatic 
+% thermal power dissipated in each subvolume, and the monochromatic thermal
+% power dissipated in each bulk object.
+
+% INPUTS:  omega            Radial frequency [rad/s]
+%          r                (N x 3) matrix containing points of all cubic lattice points of thermal objects [m]
+%          epsilon          (N x 1) vector of all subvolume dielectric functions
+%          epsilon_ref      Dielectric function for background reference medium (constant)
+%          delta_V          (N x 1) vector of all subvolume sizes [m^3]
+%          T_vector         (N x 1) vector of all subvolume temperatures [K]
+%          ind_bulk         Indices of first subvolume in a given bulk object
+%
+% OUTPUTS: G_sys_2D         System Green's function matrix for all subvolume interactions in 2D matrix format (3N x 3N)
+%          Trans            Transmission coefficient matrix (3N x 3N)
+%          Q_omega_bulk     Spectral heat dissipation within bulk objects [W/m]
+%          Q_omega_subvol   Spectral heat dissipation within subvolumes [W/m*(m^3)]
+%
+
+
+
+% Determine total number of subvolumes
+[N,~] = size(r);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate background medium Green's function %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[ ~, G_0_2D] = G_0_function(r, omega, delta_V, epsilon_ref, N, wave_type);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Populate deterministic interaction matrix %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[ A_2D, ~ ] = A_matrix_function(G_0_2D, omega, epsilon, epsilon_ref, delta_V, N);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate system Green's function %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+t1 = toc;
+G_sys_2D = A_2D\G_0_2D;  % System Green's function matrix
+t2 = toc;
+
+
+disp(['Time for matrix inversion = ' num2str(t2-t1) ' s = ' num2str((t2-t1)/60) ' minutes'])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate transmission coefficient %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+t3 = toc;
+[ Trans ] = trans_coeff_function(G_sys_2D, omega, epsilon, delta_V, N);
+t4 = toc;
+
+disp(['Time to calculate transmission coefficient = ' num2str(t4-t3) ' s = ' num2str((t4-t3)/60) ' minutes'])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Calculate monochromatic power dissipated in bulk objects %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[ Q_omega_bulk, Q_omega_subvol ] = Q_function(Trans, omega, T_vector, N, ind_bulk);
+
+
+
